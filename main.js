a039b8d1a37af4f4f760833b85dadcc798e5e818
Michael Tanaka <tanakam4214@gmail.com>
2023-05-03 14:02:26 -0700

    creates table skeleton

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const topTenCryptoApi = "https://api.coincap.io/v2/assets?limit=10"
 const elevenThroughTwentyCryptoApi="https://api.coincap.io/v2/assets?limit=10&offset=10"
-fetch(topTenCryptoApi).then(r=>r.json()).then(crypto=>console.log(crypto))
-fetch(elevenThroughTwentyCryptoApi).then(r=>r.json()).then(crypto=>console.log(crypto))
+// fetch(topTenCryptoApi).then(r=>r.json()).then(crypto=>console.log(crypto))
+// fetch(elevenThroughTwentyCryptoApi).then(r=>r.json()).then(crypto=>console.log(crypto))
